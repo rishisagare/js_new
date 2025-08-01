79114bba251dd0764e31fe0f7ed44dd52b7822f2
rishisagare <160752476+rishisagare@users.noreply.github.com>
2025-08-01 07:56:39 +0000

    COnversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33" =>33
+//"33abc"=> NaN (Not a number)
+// true=> 1;false=> 0
+// let isLoggedInt=1
+
+// let booleanIsoggedIn= Boolean(isLoggedInt)
+// console.log(booleanIsoggedIn);
+
+let Name="rishi"
+
+let booleanName= Boolean(Name)
+console.log(booleanName);
+
+// let someNumber= 33
+
+// let strinNumner = String(someNumber)
+// console.log(typeof strinNumner);
