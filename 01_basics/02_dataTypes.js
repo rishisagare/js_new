8c075c183b1ddc8e9c05faf98940d64cbf3d871e
rishisagare <160752476+rishisagare@users.noreply.github.com>
2025-08-01 07:28:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all Js code as newer version
+
+//alert(3+3) // we are using nodde js not brower
+let name="Rishi"
+let age=18
+let isLoggedInt=false
+
+//number => 2 to pwer 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined => 
+// Symbol => unquie
+
+
+// object
+
+console.log(typeof null);// obj
