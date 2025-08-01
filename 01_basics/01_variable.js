fe57107183790386edf5a3da0d31c639da4f165b
rishisagare <160752476+rishisagare@users.noreply.github.com>
2025-08-01 07:08:54 +0000

    variabe

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const account_Id = 1443322
+let account_Email ="rishisagare@gmail.com"
+var accountPass="12345"
+accountCity="Mumbai"
+let account_State;
+
+//account_Id=44// not allowed
+account_Email="tdnog@gmail.com"
+accountPass="32324"
+accountCity="Pune"
+//console.log(account_Id);
+
+
+/*
+Prefer Not to use Varr
+beacuse of issue in block cope and functional scope
+*/
+
+
+
+console.table([account_Id,account_Email,accountPass,accountCity,account_State])
+
