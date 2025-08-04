1fea2e453864eec7e90ce9082681529297aaa871
rishisagare <160752476+rishisagare@users.noreply.github.com>
2025-08-04 14:19:20 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,41 @@
+// Primitive data types 
+// 7 types : Strings, boolean , null , undefined, Symbol, BigInt(big values)
+
+const score= 100
+const scoreValue=100.3
+
+const isloogedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID=Symbol('123')
+console.log(id=== anotherID);
+
+
+const bigNumber = 234325925792034873402748783n
+
+
+
+
+// Non-Primitive(refrence type)
+
+//Array, Objects, Functions
+
+const aniCharc=["NAruto","Luffy", "Goku"]
+
+let obj={
+    name:"Rishikesh",
+    age:22,
+
+}
+
+
+const myFunction=function(){
+    console.log("Hello world");
+    
+    
+}
+
+
+
