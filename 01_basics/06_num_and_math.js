8dce8e052643657ec72c47b789863efcf35c771b
rishisagare <160752476+rishisagare@users.noreply.github.com>
2025-08-11 16:34:25 +0000

    numbers and Math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,20 @@
+// const score= 400
+// console.log(score);
+
+// const balance =new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+
+// const otherNumber=123.779364
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundereds=100000
+// console.log(hundereds.toLocaleString('en-IN'));
+//++++++++++++++++++++ MATH +++++++++++++++++++++++++
+
+console.log(Math.PI);
+console.log(Math.abs(4));
+console.log(Math.random()*100);
+
