f672b99889febea2f321033cd712f36896144cfe
rishisagare <160752476+rishisagare@users.noreply.github.com>
2025-08-19 17:18:14 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,4 +1,24 @@
 // array
 
-const myArr = [ 1, 2, 3, 4, 5, 6, 7]
-console.log(myArr[1]);
+ const myArr = [ 1, 2, 3, 4, 5, 6, 7]
+// console.log(myArr[1]);
+// // methods
+
+// // myArr.push(6)
+// // // myArr.pop()
+// // console.log(myArr.includes(6));
+// const newArr= myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArr);
+
+console.log("A",myArr);
+const myn1= myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B" , myArr);
+const myn2 = myArr.slice(1,3)
+console.log(myn2);
+
+
+
